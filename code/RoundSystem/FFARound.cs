a7b9ea2b83d65c1819c967a3d41eb5e60fd6b054
Tony Ferguson <devultj@gmail.com>
2021-04-30 10:03:45 +0100

    Round logic wip

---
+++ code/RoundSystem/FFARound.cs
@@ -0,0 +1,43 @@
+
+using Sandbox;
+using Sandbox.UI;
+using System.Threading.Tasks;
+
+namespace ElementGame
+{
+    public abstract class FFARound : BaseRound
+    {
+        public override int Length => 180;
+        public override string Name => "Free for all";
+
+        public virtual float EndGameTime => 10f;
+
+        TaskSource RoundEndTask;
+
+        public override void Begin()
+        {
+            base.Begin();
+
+            // Respawn all players
+            Player.All.ForEach( Player => Player.Respawn() );
+
+            ChatBox.AddInformation( "The game has begun." );
+        }
+
+        protected async Task HandleRoundEnd()
+        {
+            await RoundEndTask.DelaySeconds( EndGameTime );
+
+            Begin();
+        }
+
+        public override void End()
+        {
+            base.End();
+
+            ChatBox.AddInformation( $"Game over. Next round in {EndGameTime} seconds." );
+
+            _ = HandleRoundEnd();
+        }
+    }
+}
