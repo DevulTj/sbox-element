e744b78844daf4e48d927c005cd5eca92987ee4c
Tony Ferguson <devultj@gmail.com>
2021-05-31 16:17:43 +0100

    Added element_ffa_length

--- code/RoundSystem/FFA/Main.cs
+++ code/RoundSystem/FFA/Main.cs
@@ -6,7 +6,10 @@ namespace Element.FreeForAll
 {
 	public partial class MainRound : BaseRound
 	{
-		public override int Length => 20;
+		[ServerVar( "element_ffa_length", Help = "Round length for Free For All in Element" )]
+		public static int RoundLength { get; set; } = 60;
+
+		public override int Length => RoundLength;
 		public override string Name => "Free for all";
 		public override string StartMessage => "The game has begun.";
 		public override BaseRound GetNextRound() => new StatsRound();
