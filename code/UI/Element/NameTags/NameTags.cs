957dbe5b73d9c333cc5a0d3ea3145d0b5d7e6e0f
Tony Ferguson <devultj@gmail.com>
2021-06-05 15:07:54 +0100

    Moved nametags in to Element

---
+++ code/UI/Element/NameTags/NameTags.cs
@@ -0,0 +1,169 @@
+﻿using Sandbox.UI.Construct;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Sandbox.UI;
+using Sandbox;
+
+namespace Element.UI
+{
+	public class BaseNameTag : Panel
+	{
+		public Label NameLabel;
+		public Image Avatar;
+
+		Player player;
+
+		public BaseNameTag( Player player )
+		{
+			this.player = player;
+
+			var client = player.GetClientOwner();
+
+			NameLabel = Add.Label( $"{client.Name}" );
+			Avatar = Add.Image( $"avatar:{client.SteamId}" );
+		}
+
+		public virtual void UpdateFromPlayer( Player player )
+		{
+			// Nothing to do unless we're showing health and shit
+		}
+	}
+
+	public class NameTags : Panel
+	{
+		Dictionary<Player, BaseNameTag> ActiveTags = new Dictionary<Player, BaseNameTag>();
+
+		public float MaxDrawDistance = 400;
+		public int MaxTagsToShow = 5;
+
+		public NameTags()
+		{
+			StyleSheet.Load( "/ui/element/nametags/NameTags.scss" );
+		}
+
+		public override void Tick()
+		{
+			base.Tick();
+
+
+			var deleteList = new List<Player>();
+			deleteList.AddRange( ActiveTags.Keys );
+
+			int count = 0;
+			foreach ( var player in Entity.All.OfType<Player>().OrderBy( x => Vector3.DistanceBetween( x.Position, CurrentView.Position ) ) )
+			{
+				if ( UpdateNameTag( player ) )
+				{
+					deleteList.Remove( player );
+					count++;
+				}
+
+				if ( count >= MaxTagsToShow )
+					break;
+			}
+
+			foreach( var player in deleteList )
+			{
+				ActiveTags[player].Delete();
+				ActiveTags.Remove( player );
+			}
+
+		}
+
+		public virtual BaseNameTag CreateNameTag( Player player )
+		{
+			if ( player.GetClientOwner() == null )
+				return null;
+
+			var tag = new BaseNameTag( player );
+			tag.Parent = this;
+			return tag;
+		}
+
+		public bool UpdateNameTag( Player player )
+		{
+			// Don't draw local player
+			if ( player == Local.Pawn )
+				return false;
+
+			if ( player.LifeState != LifeState.Alive )
+				return false;
+
+			//
+			// Where we putting the label, in world coords
+			//
+			var head = player.GetAttachment( "hat" );
+			if ( head.Position == Vector3.Zero )
+				head.Position = player.EyePos;
+
+			var labelPos = head.Position + Vector3.Up * 5;
+
+
+			//
+			// Are we too far away?
+			//
+			float dist = labelPos.Distance( CurrentView.Position );
+			if ( dist > MaxDrawDistance )
+				return false;
+
+			//
+			// Are we looking in this direction?
+			//
+			var lookDir = (labelPos - CurrentView.Position).Normal;
+			if ( CurrentView.Rotation.Forward.Dot( lookDir ) < 0.5 )
+				return false;
+
+			// TODO - can we see them
+
+
+			MaxDrawDistance = 400;
+
+			// Max Draw Distance
+
+
+			var alpha = dist.LerpInverse( MaxDrawDistance, MaxDrawDistance * 0.1f, true );
+
+			// If I understood this I'd make it proper function
+			var objectSize = 0.05f / dist / (2.0f * MathF.Tan( (CurrentView.FieldOfView / 2.0f).DegreeToRadian() )) * 1500.0f;
+
+			objectSize = objectSize.Clamp( 0.05f, 1.0f );
+
+			if ( !ActiveTags.TryGetValue( player, out var tag ) )
+			{
+				tag = CreateNameTag( player );
+				if ( tag != null )
+				{
+					ActiveTags[player] = tag;
+				}
+			}
+
+			tag.UpdateFromPlayer( player );
+
+			var screenPos = labelPos.ToScreen();
+
+			tag.Style.Left = Length.Fraction( screenPos.x );
+			tag.Style.Top = Length.Fraction( screenPos.y );
+
+			var checkVisibilityTrace = Trace
+				.Ray( Local.Pawn.EyePos, player.EyePos )
+				.WorldOnly()
+				.Run();
+						// And this
+			tag.Style.Opacity = !checkVisibilityTrace.Hit && checkVisibilityTrace.Entity != player ? alpha : 0f;
+
+			var transform = new PanelTransform();
+			transform.AddTranslateY( Length.Fraction( -1.0f ) );
+			transform.AddScale( objectSize );
+			transform.AddTranslateX( Length.Fraction( -0.5f ) );
+
+			tag.Style.Transform = transform;
+			tag.Style.Dirty();
+
+			return true;
+		}
+	}
+}
