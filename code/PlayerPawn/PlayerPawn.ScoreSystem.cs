3efa25d50c2d59523833b40bc5d69e179ea13826
Tony Ferguson <devultj@gmail.com>
2021-06-05 12:45:57 +0100

    Started work on ScoreSystem

---
+++ code/PlayerPawn/PlayerPawn.ScoreSystem.cs
@@ -0,0 +1,29 @@
+﻿using Sandbox;
+using System;
+
+namespace Element
+{
+	public partial class PlayerStats : NetworkComponent
+	{
+		[Net] public int Kills { get; set; }
+		[Net] public int Deaths { get; set; }
+
+		public void Clear()
+		{
+			if ( Host.IsServer )
+			{
+				Kills = 0;
+				Deaths = 0;
+			}
+		}
+
+		public int GetKills() => Kills;
+		public int GetDeaths() => Deaths;
+		public float GetKillDeathRatio() => Convert.ToSingle( Kills ) / Convert.ToSingle( Deaths );
+	}
+
+	partial class PlayerPawn
+	{
+		[Net] public PlayerStats Stats { get; private set; }
+	}
+}
