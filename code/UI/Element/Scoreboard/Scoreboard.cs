ba5d6f46d243316018974f022dc9ad4da4be43a7
Tony Ferguson <devultj@gmail.com>
2021-06-05 13:42:19 +0100

    Moved the scoreboard

---
+++ code/UI/Element/Scoreboard/Scoreboard.cs
@@ -0,0 +1,37 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Element.UI
+{
+	public class Scoreboard : Sandbox.UI.Scoreboard<ScoreboardEntry>
+	{
+
+		public Scoreboard()
+		{
+			StyleSheet.Load( "/UI/Element/Scoreboard/Scoreboard.scss" );
+		}
+
+		protected override void AddHeader()
+		{
+			Header = Add.Panel( "header" );
+			Header.Add.Label( "name", "name" );
+			Header.Add.Label( "kills", "kills" );
+			Header.Add.Label( "deaths", "deaths" );
+			Header.Add.Label( "ping", "ping" );
+		}
+	}
+
+	public class ScoreboardEntry : Sandbox.UI.ScoreboardEntry
+	{
+		public ScoreboardEntry()
+		{
+		}
+
+		public override void UpdateFrom( PlayerScore.Entry entry )
+		{
+			base.UpdateFrom( entry );
+		}
+	}
+}
